3391e117b14fdfd0c70f5d20ef1f31be168d6c7c
donggi-lee-bit <devdonggilee@gmail.com>
2022-09-28 12:54:24 +0900

    feat: POST 요청 테스트 추가

--- src/test/java/http/HttpRequestTest.java
+++ src/test/java/http/HttpRequestTest.java
@@ -3,29 +3,34 @@ package http;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import http.request.HttpMethod;
+import http.request.HttpRequest;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class HttpRequestTest {
 
     private String testDirectory = "./src/test/resources/";
 
-    private HttpRequest httpRequest;
-
-    @BeforeEach
-    void setUp() throws FileNotFoundException {
+    @Test
+    void request_GET() throws FileNotFoundException {
         InputStream in = new FileInputStream(
             new File(testDirectory + "Http_GET.txt"));
-        httpRequest = new HttpRequest(in);
+        HttpRequest httpRequest = new HttpRequest(in);
+        assertThat(HttpMethod.GET).isEqualTo(httpRequest.getMethod());
+        assertThat("/user/create").isEqualTo(httpRequest.getPath());
+        assertThat("keep-alive").isEqualTo(httpRequest.getHeader("Connection"));
+        assertThat("javajigi").isEqualTo(httpRequest.getParameter("userId"));
     }
 
     @Test
-    void request_GET() throws FileNotFoundException {
-        assertThat(HttpMethod.GET).isEqualTo(httpRequest.getMethod());
+    void request_POST() throws FileNotFoundException {
+        InputStream in = new FileInputStream(
+            new File(testDirectory + "Http_POST.txt"));
+        HttpRequest httpRequest = new HttpRequest(in);
+        assertThat(HttpMethod.POST).isEqualTo(httpRequest.getMethod());
         assertThat("/user/create").isEqualTo(httpRequest.getPath());
         assertThat("keep-alive").isEqualTo(httpRequest.getHeader("Connection"));
         assertThat("javajigi").isEqualTo(httpRequest.getParameter("userId"));
