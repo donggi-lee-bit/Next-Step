7b9a662afa106098aa434020e4a1bea9582a9e37
donggi-lee-bit <devdonggilee@gmail.com>
2022-10-11 15:27:47 +0900

    feat: Session 추가

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -7,8 +7,11 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.util.Map;
+import java.util.UUID;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import util.HttpRequestUtils;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -28,16 +31,25 @@ public class RequestHandler extends Thread {
             HttpRequest request = new HttpRequest(in);
             HttpResponse response = new HttpResponse(out);
 
+            if (getSessionId(request.getHeader("Cookie")) == null) {
+                response.addHeader("Set-Cookie", "JSESSIONID=" + UUID.randomUUID());
+            }
+
             Controller controller = RequestMapping.getController(request.getPath());
             if (controller == null) {
                 String path = getDefaultPath(request.getPath());
-                response.forwardBody(path);
+                response.forward(path);
             } else {
                 controller.service(request, response);
             }
         } catch (IOException e) {
             log.error(e.getMessage());
         }
+    }
+
+    private String getSessionId(String cookie) {
+        Map<String, String> cookies = HttpRequestUtils.parseCookies(cookie);
+        return cookies.get("JSESSIONID");
     }
 
     private String getDefaultPath(String path) {
