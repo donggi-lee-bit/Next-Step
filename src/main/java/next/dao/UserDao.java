1481f6a034108338ad669d02a05e6332f9515377
donggi-lee-bit <devdonggilee@gmail.com>
2022-10-18 12:36:56 +0900

    feat: update 메서드 구현

--- src/main/java/next/dao/UserDao.java
+++ src/main/java/next/dao/UserDao.java
@@ -100,4 +100,31 @@ public class UserDao {
             }
         }
     }
+
+    public void update(User user) throws SQLException {
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        try {
+
+            con = ConnectionManager.getConnection();
+            String sql = "UPDATE USERS SET password = ?, name = ?, email = ? WHERE userId = ?";
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(4, user.getUserId());
+            pstmt.setString(1, user.getPassword());
+            pstmt.setString(2, user.getName());
+            pstmt.setString(3, user.getEmail());
+            pstmt.executeUpdate();
+        } finally {
+            if (rs != null) {
+                rs.close();
+            }
+            if (pstmt != null) {
+                pstmt.close();
+            }
+            if (con != null) {
+                con.close();
+            }
+        }
+    }
 }
--- src/test/java/next/dao/UserDaoTest.java
+++ src/test/java/next/dao/UserDaoTest.java
@@ -1,22 +1,55 @@
 package next.dao;
 
-
-
 import static org.assertj.core.api.Assertions.assertThat;
 
-import next.model.User;
+import java.sql.SQLException;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+
+import core.jdbc.ConnectionManager;
+import next.model.User;
 
 public class UserDaoTest {
 
+    @BeforeEach
+    public void setup() {
+        ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+        populator.addScript(new ClassPathResource("jwp.sql"));
+        DatabasePopulatorUtils.execute(populator, ConnectionManager.getDataSource());
+    }
+
     @Test
-    public void crud() throws Exception {
+    public void insert() throws SQLException {
         User expected = new User("userId", "password", "name", "javajigi@email.com");
         UserDao userDao = new UserDao();
         userDao.insert(expected);
-
         User actual = userDao.findByUserId(expected.getUserId());
         assertThat(actual).isEqualTo(expected);
     }
 
+    @Test
+    public void findAll() throws SQLException {
+        UserDao userDao = new UserDao();
+        List<User> users = userDao.findAll();
+        assertThat(users.size()).isEqualTo(1);
+    }
+
+    @Test
+    void update() throws SQLException {
+        User user = new User("userId", "password", "name", "javajigi@email.com");
+        UserDao userDao = new UserDao();
+        userDao.insert(user);
+        User findUser = userDao.findByUserId(user.getUserId());
+        assertThat(user).isEqualTo(findUser);
+
+        user.update(new User("userId", "password2", "name2", "sanjigi@email.com"));
+        userDao.update(user);
+        findUser = userDao.findByUserId(user.getUserId());
+        assertThat(user).isEqualTo(findUser);
+    }
 }
